fa70a722b94a7aac2c2ffe1abbf5d93c34c89a91
Israel Oliveira <102741887+IsraelOliver@users.noreply.github.com>
2024-01-28 12:09:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const resultArtist = document.getElementById("result-artist");
+const playlistContainer = document.getElementById("result-playlists");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+  fetch`http://localhost:3000/artists`
+    .then((response) => response.json())
+    .then((results) => filter(results, searchTerm
+      ))
+    .then((results) => displayResults(results));
+}
+
+async function filter( list, searchTerm ) {
+    return list.filter((item) => item.name.toLowerCase().startsWith(searchTerm));
+
+}
+
+function displayResults(results) {
+  hidePlaylists();
+  const artistImage = document.getElementById("artist-img");
+  const artistName = document.getElementById("artist-name");
+
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+    console.log('fdp')
+  });
+  resultArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.classList.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
+});
